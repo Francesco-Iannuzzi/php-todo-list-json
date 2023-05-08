b6b5a13fd555392adc68290bf6b935d84e34e703
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-05-08 14:48:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+/*
+
+Descrizione:
+Dobbiamo creare una web-app che permetta di leggere e scrivere una lista di Todo.
+Deve essere anche gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
+
+Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+
+Consigli:
+Nello svolgere l’esercizio seguite un approccio graduale.
+- Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+
+- Lo step successivo è quello di “testare" l'invio di un nuovo task. Iniziate prima da un array in pagina ma Attenzione! É solo leggendo e scrivendo su un file json come fatto in classe che avremo la persistenza dei dati.
+
+- Solo a questo punto sarà utile passare alla lettura/scrittura della lista da un file JSON.
+
+Bonus
+- Mostrare lo stato del task → se completato, barrare il testo
+- Permettere di segnare un task come completato facendo click sul testo
+- Permettere il toggle del task (completato/non completato)
+- Abilitare l’eliminazione di un task
+
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <!-- /my style -->
+    <title>Todo List PHP</title>
+</head>
+
+<body>
+    <h1>ciao</h1>
+
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <!-- /vue script -->
+
+</body>
+
+</html>
